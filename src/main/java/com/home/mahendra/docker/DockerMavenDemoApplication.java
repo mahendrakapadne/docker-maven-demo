b4edd1fd4c31d28d895728c9538f28a837cb8911
Mahendra Kapadne <Mahendra Kapadne>
2017-03-04 21:13:55 +0800

    First Draft

---
+++ src/main/java/com/home/mahendra/docker/DockerMavenDemoApplication.java
@@ -0,0 +1,12 @@
+package com.home.mahendra.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerMavenDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/home/mahendra/docker/DockerMavenDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.home.mahendra.docker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerMavenDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
